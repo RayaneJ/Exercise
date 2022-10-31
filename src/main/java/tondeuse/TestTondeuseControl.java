03b757c84bcb251a773aab378ecae41bd9522c0c
RayaneJ <rayan_dif@outlook.com>
2022-10-31 18:26:54 +0100

    refactoring

---
+++ src/main/java/tondeuse/TestTondeuseControl.java
@@ -0,0 +1,68 @@
+package tondeuse;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import tondeuse.CommandeInvalideException;
+import tondeuse.LimitePelouseException;
+import tondeuse.Orientation;
+import tondeuse.PointCardinal;
+import tondeuse.Position;
+import tondeuse.Tondeuse;
+import tondeuse.TondeuseControl;
+
+public class TestTondeuseControl {
+Position pos1 ;
+Position pos2;
+PointCardinal P1;
+PointCardinal P2;
+PointCardinal P3;
+PointCardinal P4;
+
+
+
+ 
+
+    @Test
+    public void shouldControTondeuse() throws  LimitePelouseException, CommandeInvalideException {
+
+      
+        Tondeuse T1;
+        String commande1;
+        Tondeuse T2;
+        String commande2;
+        int pelousePosMax;
+
+        TondeuseControl tondeuseControl;
+
+        tondeuseControl= new TondeuseControl();
+        P1 = new PointCardinal(1);
+        P2 = new PointCardinal(2);
+        P3 = new PointCardinal(3);
+        P4 = new PointCardinal(3);
+        
+        
+        pos1 = new Position (P1,P2);
+        pos2 = new Position (P3,P3);
+
+
+        T1 = new Tondeuse(pos1, Orientation.N);
+        commande1= "GAGAGAGAA";
+        T2= new Tondeuse(pos2, Orientation.E);
+        commande2 = "AADAADADDA";
+        pelousePosMax = 5;
+    	
+        Tondeuse tond = new Tondeuse(new Position(new PointCardinal(1), new PointCardinal(3)), Orientation.N);
+    	
+
+        tondeuseControl.controlerTondeuse(pelousePosMax, T1, commande1);
+        assertEquals(T1, tond);
+        
+        
+       
+
+ 
+    }
+
+}
---
+++ src/main/java/tondeuse/TondeuseControl.java
@@ -0,0 +1,41 @@
+package tondeuse;
+public class TondeuseControl {
+
+    public TondeuseControl() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void  controlerTondeuse(int pelousePositionMax, Tondeuse tondeuse, String commandes) throws CommandeInvalideException, LimitePelouseException
+            {
+
+        for (char commande : commandes.toCharArray()) {
+        	
+    
+                switch(Character.toUpperCase(commande)) {
+                    case 'A':
+                        tondeuse.avancer(pelousePositionMax);
+                    break;
+                    case 'G':
+                        tondeuse.setOrientationGauche();
+                       break;
+                    case 'D':
+                        tondeuse.setOrientationDroite();
+                        break;
+                     
+                    default:
+                        throw new CommandeInvalideException("commande inconnu");
+                }
+            }   	
+        	
+        }
+          
+        }
+    
+    
+
+
+        
+
+
+   
